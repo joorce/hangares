5ea24cefd96d1e1299d2e6fbc533b35f6a9cc6b2
Jose Ortega <joorce@gmail.com>
2018-02-17 10:16:44 +0100

    Más cambios

--- Hangares.java
+++ Hangares.java
@@ -1,19 +1,42 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Hangares {
     public static void main(String[] args) {
-	int numHangares = 2;
-	int[] hangares = {12, 20};
-	int numNaves = 3;
-	int[] naves = {10, 11, 10};
-	
-	for(int i = 0; i < numNaves; i++) {
-	    sortHangares(hangares);
-	    int nave = naves[i];
-	    aparcaNave(nave, hangares);
-	}
+	Scanner stdin = new Scanner(System.in);
+	boolean shouldAsk = true; 
+	int numHangares = 0;
+	int[] hangares;
+	int numNaves;
+	int[] naves;
 
-	System.out.println(checkHangares(hangares));
+	while(shouldAsk){
+	    int entrada = stdin.nextInt();
+	    if(entrada == 0) {
+		shouldAsk = false;
+	    } else {
+		numHangares = entrada;
+		hangares = new int[numHangares];
+		for(int i = 0; i < numHangares; i++) {
+		    hangares[i] = stdin.nextInt();
+		}
+		numNaves = stdin.nextInt();
+		naves = new int[numNaves];
+		for(int i = 0; i < numNaves; i++) {
+		    naves[i] = stdin.nextInt();
+		}
+		
+		for(int i = 0; i < numNaves; i++) {
+		    sortHangares(hangares);
+		    int nave = naves[i];
+		    aparcaNave(nave, hangares);
+		}
+
+		System.out.println(checkHangares(hangares));
+
+	    }
+	}
+	
     }
 
     public static void sortHangares(int[] hangares) {
